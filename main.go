4c3facf44db55963be1f95bf470524c6f6b95eca
Bob Campbell <mostlybob@gmail.com>
2014-09-16 05:45:32 -0500

    first go with some variables

--- main.go
+++ main.go
@@ -3,7 +3,17 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("go")
+
+	//2.1 Variables & inferred typing
+	var (
+		name     string
+		age      int
+		location string
+	)
+
+	name = "Prince Oberyn"
+	age = 32
+	location = "Dorne"
+
+	fmt.Printf("%s (%d) of %s", name, age, location)
 }
-
-
