64fa3bdfe4b44928db6d3dc9c3318a7a63c16462
Bob Campbell <mostlybob@gmail.com>
2014-09-16 06:05:31 -0500

    short declaration syntax for variable initialization

--- main.go
+++ main.go
@@ -5,11 +5,8 @@ import "fmt"
 func main() {
 
 	//2.1 Variables & inferred typing
-	var (
-		name     = "Prince Oberyn"
-		age      = 32
-		location = "Dorne"
-	)
+	name, location := "Prince Oberyn", "Dorne"
+	age := 32
 
 	fmt.Printf("%s (%d) of %s", name, age, location)
 }
